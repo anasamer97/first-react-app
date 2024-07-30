ef445c9bda59eb58df7aaa34d69c9ce4f75049c1
Anas Amer <anasamer97@gmail.com>
2024-07-30 18:36:12 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import pages from 'vite-plugin-react-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/reactapp1/",
+  plugins: [react(), pages()],
+  base: "/first-react-app/",
 })
